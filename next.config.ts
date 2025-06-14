e81a907f4f37e36e70a8fd17bc8965ec9484ba9a
nrbnayon <nrbnayon@gmail.com>
2025-06-14 15:06:37 +0600

    Okay

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    optimizeCss: true,
+  },
+  webpack: (config: any) => {
+    config.optimization.splitChunks = {
+      chunks: "all",
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
